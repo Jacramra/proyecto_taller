9514f718bfec6de3c7094bd1b394cd8929e4c2a3
Jacramra <63827359+Jacramra@users.noreply.github.com>
2025-05-28 01:00:49 -0500

    Se creo clase Participante y se creo Diccionario precios.

--- participante.py
+++ participante.py
@@ -1,2 +1,10 @@
 import pandas as pd
 import matplotlib as plt
+
+class Participante:
+    #Diccionario precios: indica taller artístico vs su precio
+    precios = {
+        "Pintura": 6000,
+        "Teatro": 8000,
+        "Danza": 7000
+    }
