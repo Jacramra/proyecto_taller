170db115b2b491709384b9d505b85b2fdd4be1fd
Jacramra <63827359+Jacramra@users.noreply.github.com>
2025-05-28 01:22:42 -0500

    Ajusta automáticamente el espaciado del gráfico.

--- interfaz.py
+++ interfaz.py
@@ -59,7 +59,7 @@ def generar_reporte():
     # Gráfico
     conteo = df["Taller"].value_counts()
     conteo.plot(kind="bar", title="Inscritos por Taller", ylabel="Cantidad")
-    
+    plt.tight_layout()
 
 #---------------------Interfaz---------------------
 
