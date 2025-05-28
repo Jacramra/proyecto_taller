9b1bb23e04d562682955a75e7b15392b8aff7bd0
Jacramra <63827359+Jacramra@users.noreply.github.com>
2025-05-28 00:02:51 -0500

    Adicionando función Registrar.

--- interfaz.py
+++ interfaz.py
@@ -7,27 +7,33 @@ import matplotlib as plt
 ventana = tk.Tk()
 ventana.title("Talleres Artísticos")
 
+# Función para registrar persona
+def registrar():
+    nombre = entry_nombre.get()
+    edad = entry_edad.get()
+    taller = combo_taller.get()
+    clases = entry_clases.get()
 
 # Campo: Nombre
-label_nombre = tk.Label(ventana, text="Nombre")                             # Mostrar etiqueta nombre
+label_nombre = tk.Label(ventana, text="Nombre:")                             # Mostrar etiqueta nombre
 label_nombre.grid(row=0, column=0)          
 entry_nombre = tk.Entry(ventana)                                            # Mostrar campo de texto
 entry_nombre.grid(row=1, column=1)
 
 # Campo: Nombre
-label_edad = tk.Label(ventana, text="Edad")
+label_edad = tk.Label(ventana, text="Edad:")
 label_edad.grid(row=1, column=0)                                            # Mostrar etiqueta edad
 entry_edad = tk.Entry(ventana)
 entry_edad.grid(row=1, column=1)                                            # Mostrar campo de texto
 
 # Campo: Taller inscrito
-label_taller = tk.Label(ventana, text="Taller")                              # Mostrar etiqueta taller
+label_taller = tk.Label(ventana, text="Taller inscrito:")                    # Mostrar etiqueta taller
 label_taller.grid(row=2, column=0)
 combo_taller = ttk.Combobox(ventana, values=["Pintura", "Teatro", "Danza"])  # Mostrar lista de opciones
 combo_taller.grid(row=2, column=1)
 
 # Campo: Número de clases tomadas
-label_clases = tk.Label(ventana, text="Número de Clases")                    # Mostrar etiqueta número de clases
+label_clases = tk.Label(ventana, text="Número de claes:")                    # Mostrar etiqueta número de clases
 label_clases.grid(row=3, column=0)
 entry_clases = tk.Entry(ventana)                                             # Mostrar campo de texto
 entry_clases.grid(row=3, column=1)
