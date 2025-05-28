74e6d7c48f33714aa6b163d2c00b15cc57216f8d
Jacramra <63827359+Jacramra@users.noreply.github.com>
2025-05-27 23:44:57 -0500

    Adicionando botones.

--- interfaz.py
+++ interfaz.py
@@ -7,5 +7,15 @@ import matplotlib as plt
 ventana = tk.Tk()
 ventana.title("Talleres Artísticos")
 
+# Botón para registrar persona
+boton_registrar = tk.Button(ventana, text="Registrar persona", command=none)
+boton_registrar.grid(row=4, column=0, pady=10)
+
+# Botón para generar reporte
+boton_reporte = tk.Button(ventana, text="Reporte", command=none)
+boton_reporte.grid(row=4, column=1)
+
+ventana.mainloop()
+
 
 
