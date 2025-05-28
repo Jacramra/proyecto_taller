be5737704d9a16582e9df36ba3ff283e68c49d4e
Jacramra <63827359+Jacramra@users.noreply.github.com>
2025-05-28 01:04:14 -0500

    Se crea el método especial __init__ de Python. Se ejecuta automáticamente cuando creas un nuevo objeto de la clase.

--- participante.py
+++ participante.py
@@ -8,3 +8,10 @@ class Participante:
         "Teatro": 8000,
         "Danza": 7000
     }
+    
+    #Método de Python: crep un nuevo objeto de la clase
+    def __init__(self, nombre, edad, taller, clases):
+        self.nombre = nombre
+        self.edad = int(edad)
+        self.taller = taller
+        self.clases = int(clases)
